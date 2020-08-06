4c3b36ddf7bc8a463a4ef6c19779cb38157060bd
PauloR1997 <oyepaulo57@gmail.com>
2020-08-06 14:18:12 -0500

    paulo

--- src/java/Modelo/mensaje.java
+++ src/java/Modelo/mensaje.java
@@ -10,11 +10,71 @@ package Modelo;
  * @author Paty Ramirez
  */
 public class mensaje {
-     int idmensaje;
-     String sugerencia;
-     String fecha;
-     String destinatario;
-     String remitente;
-     String folio;
+     private int idmensaje;
+     private String sugerencia;
+     private String fecha;
+     private String destinatario;
+     private String remitente;
+     private String folio;
+
+    public mensaje() {
+    }
+
+    public mensaje(int idmensaje, String sugerencia, String fecha, String destinatario, String remitente, String folio) {
+        this.idmensaje = idmensaje;
+        this.sugerencia = sugerencia;
+        this.fecha = fecha;
+        this.destinatario = destinatario;
+        this.remitente = remitente;
+        this.folio = folio;
+    }
+
+    public String getFolio() {
+        return folio;
+    }
+
+    public void setFolio(String folio) {
+        this.folio = folio;
+    }
+
+    public int getIdmensaje() {
+        return idmensaje;
+    }
+
+    public void setIdmensaje(int idmensaje) {
+        this.idmensaje = idmensaje;
+    }
+
+    public String getSugerencia() {
+        return sugerencia;
+    }
+
+    public void setSugerencia(String sugerencia) {
+        this.sugerencia = sugerencia;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public String getRemitente() {
+        return remitente;
+    }
+
+    public void setRemitente(String remitente) {
+        this.remitente = remitente;
+    }
      
 }
--- src/java/Modelo/persona.java
+++ src/java/Modelo/persona.java
@@ -10,10 +10,72 @@ package Modelo;
  * @author Paty Ramirez
  */
 public class persona {
-    int idpersona;
-     String nombre;
-     String apellido;
-     String dni;
-     String telefono;
-     String pais;
+    private int idpersona;
+     private String nombre;
+     private String apellido;
+     private String dni;
+     private String telefono;
+     private String pais;
+
+    public persona() {
+    }
+     
+
+    public persona(int idpersona, String nombre, String apellido, String dni, String telefono, String pais) {
+        this.idpersona = idpersona;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.pais = pais;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getIdpersona() {
+        return idpersona;
+    }
+
+    public void setIdpersona(int idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+     
 }
